33f09cacb34bab7044cf59e137bf2faab2ab0fa2
nidhogg <aripekka.kantola@gmail.com>
2025-01-20 19:42:46 +0200

    Implemented: - Dynamic command loading. - Dynamic slash command registration. - Created sample help command.

---
+++ bot/commands/util/help.js
@@ -0,0 +1,23 @@
+const { SlashCommandBuilder, Message, MessageFlags } = require('discord.js');
+
+const message =
+    `Hello! I am the Axolbot! As your humble servant, I am here to assist you with all your temple needs!
+
+Heres some links to start with:
+
+:butterfly:     **[Temple Website](https://templeofchrysalis.com/)**
+:butterfly:     **[The principle](https://templeofchrysalis.com/principles/)**
+:butterfly:     **[The Edict](https://templeofchrysalis.com/the-temple/the-edict/)**
+:butterfly:     **[Kaleidoscopes](https://templeofchrysalis.com/kaleidoscopes/)**
+:butterfly:     **[Reality shaping](https://templeofchrysalis.com/reality-shaping/)**
+:butterfly:     **[Chrysoteria](https://templeofchrysalis.com/chrysoteria/)**
+
+Don't forget to check out the <#CHANNEL_ID> and <#CHANNEL_ID> channels for more information!`;
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('help')
+        .setDescription('Give some initial help to the user ie. where to find info about temple'),
+    async execute(interaction) {
+        await interaction.reply({ content: message, flags: MessageFlags.Ephemeral });
+    }
+};
--- bot/main.js
+++ bot/main.js
@@ -1,7 +1,16 @@
-// Require the necessary modules for basic functionality
+//////////////////////////////////////
+// Imports section
+//////////////////////////////////////
+
 require('dotenv').config()
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits, MessageFlags } = require('discord.js');
 const axios = require('axios');
+
+//////////////////////////////////////
+// Client section
+//////////////////////////////////////
 
 // Create a new client instance
 const client = new Client({
@@ -13,10 +22,34 @@ const client = new Client({
     ]
 });
 
+// Dynamically read all command files from the commands directory
+
+client.commands = new Collection();
+const folderPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(folderPath);
+
+for (const folder of commandFolders) {
+    const commandsPath = path.join(folderPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        if ('data' in command && 'execute' in command) {
+            client.commands.set(command.data.name, command);
+        } else {
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
+    }
+}
+
 // When the client is ready, run this code (only once).
 client.once(Events.ClientReady, readyClient => {
     console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
+
+//////////////////////////////////////
+// General functions section ie. logger
+//////////////////////////////////////
 
 // Function to send a POST request to the backend
 async function sendPostRequest(guildId, channelId, channelName, userId, username, nickname) {
@@ -36,6 +69,10 @@ async function sendPostRequest(guildId, channelId, channelName, userId, username
     }
 }
 
+//////////////////////////////////////
+// Event listeners section
+//////////////////////////////////////
+
 // Listen and log messages to mongodb
 client.on('messageCreate', message => {
     // Prevent the bot from responding and counting its own messages
@@ -51,6 +88,29 @@ client.on('messageCreate', message => {
 
     sendPostRequest(guildId, channelId, channelName, userId, username, nickname);
 });
+
+// Listen interactions and run the appropriate command
+client.on(Events.InteractionCreate, async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    if (!command) {
+        console.error(`No command matching ${interaction.commandName} was found.`);
+        return;
+    }
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        if (interaction.replied || interaction.deferred) {
+            await interaction.followUp({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+        } else {
+            await interaction.reply({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+        }
+    }
+});
+
 
 // Log in to Discord with client's token
 client.login(process.env.APP_TOKEN); 
