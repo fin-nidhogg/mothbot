bce3845b1fba0a6bf0360ecb9439155d08e7ffcb
Ari-Pekka Kantola <aripekka.kantola@gmail.com>
2025-03-03 18:13:15 +0100

    Update API endpoints for posting user and general stats

--- bot/utils/postStats.js
+++ bot/utils/postStats.js
@@ -4,7 +4,7 @@ const config = require('../config'); // Import the config file
 // Function to send a POST request to the bot's own db API for user data
 async function postUserdata(guildId, channelId, channelName, userId, username, nickname) {
     try {
-        const response = await axios.post(`${config.apiUrl}:${config.apiPort}/add`, {
+        const response = await axios.post(`${config.apiUrl}:${config.apiPort}/user-stats/add`, {
             guildId,
             channelId,
             channelName,
@@ -21,7 +21,7 @@ async function postUserdata(guildId, channelId, channelName, userId, username, n
 // Function to send a POST request to the bot's own db API for general stats
 async function postGeneralStats(guildId, channelId, channelName) {
     try {
-        const response = await axios.post(`${config.apiUrl}:${config.apiPort}/add-general`, {
+        const response = await axios.post(`${config.apiUrl}:${config.apiPort}/general-stats/add`, {
             guildId,
             channelId,
             channelName,
