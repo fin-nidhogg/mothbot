20d5345c8ce0e1d99e156b5f276fe8a00a92d443
Ari-Pekka Kantola <aripekka.kantola@gmail.com>
2025-04-06 15:24:13 +0200

    Refactor bot readiness logging: Simplify API URL logging and include run mode in startup messages

--- bot/events/ready.js
+++ bot/events/ready.js
@@ -1,11 +1,13 @@
 const config = require('../config');
+const runmode = process.env.NODE_ENV;
 
 module.exports = {
     name: 'ready',
     once: true,
     execute(client) {
+        console.log('Bot started in ' + runmode + ' mode');
         console.log(`Ready! Logged in as ${client.user.tag}`);
-        console.log(`Bot is using API: ${config.apiUrl}:${config.apiPort}`);
+        console.log(`Bot is using API: ${config.apiUrl}`);
         console.log(`Horde generation is ${config.hordeEnabled ? 'enabled' : 'disabled'}`);
     },
 };
